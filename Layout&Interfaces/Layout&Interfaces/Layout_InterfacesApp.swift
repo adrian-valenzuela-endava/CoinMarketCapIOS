0640c48b899e5fa9b47d29f4a0293ae1a731c6f2
Nicolas <nico.marchioli@gmail.com>
2023-05-19 11:49:23 -0300

    adding project base to folder

---
+++ Layout&Interfaces/Layout&Interfaces/Layout_InterfacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_InterfacesApp.swift
+//  Layout&Interfaces
+//
+//  Created by Orlando Nicolas Marchioli on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Layout_InterfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
