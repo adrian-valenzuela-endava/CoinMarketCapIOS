ea40d69ced0f8f3a011aa4ab1fe787190a7c6a4d
Nicolas <nico.marchioli@gmail.com>
2023-05-23 17:35:04 -0300

    setting up project in develop

---
+++ UdemyCourse/UdemyCourse/UdemyCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyCourseApp.swift
+//  UdemyCourse
+//
+//  Created by Orlando Nicolas Marchioli on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UdemyCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
