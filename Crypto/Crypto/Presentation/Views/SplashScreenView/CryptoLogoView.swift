6269f218b2dbc58a44d450840d3e716241be4974
Nicolas <nico.marchioli@gmail.com>
2023-11-10 17:07:41 -0300

    reference problem solved

--- Crypto/Crypto/Presentation/Views/SplashScreenView/CryptoLogoView.swift
+++ Crypto/Crypto/Presentation/Views/SplashScreenView/CryptoLogoView.swift
@@ -1,18 +1,14 @@
-//
-//  CryptoLogoView.swift
-//  Crypto
-//
-//  Created by Orlando Nicolas Marchioli on 10/11/2023.
-//
-
 import SwiftUI
 
 struct CryptoLogoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("HomeImage").frame(maxWidth: .infinity,maxHeight: .infinity)
+            .background(Color("MainPageBackgroudColor"))
     }
 }
 
-#Preview {
-    CryptoLogoView()
+struct CryptoLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CryptoLogoView()
+    }
 }
