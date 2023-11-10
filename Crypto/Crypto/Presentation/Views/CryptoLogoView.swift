da43df01f55ac9bd4fe097efc241ca259dbdb5f8
Nicolas <nico.marchioli@gmail.com>
2023-11-10 16:58:59 -0300

    change

--- Crypto/Crypto/Presentation/Views/CryptoLogoView.swift
+++ Crypto/Crypto/Presentation/Views/CryptoLogoView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CryptoLogoView: View {
     var body: some View {
         Image("HomeImage").frame(maxWidth: .infinity,maxHeight: .infinity)
-            .background(Color("MainPageBackgroudColor"))
+            .background(Color("MainPageBackgroudColor")) 
     }
 }
 
